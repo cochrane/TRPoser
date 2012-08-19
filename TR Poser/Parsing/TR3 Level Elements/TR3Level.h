e70ffd3492670a6638ac611dba80a96a65036f42
cochrane <torsten@ferroequinologist.de>
2012-08-20 01:29:29 +0200

    Version of level specific to Tomb Raider 3.
    Main changes here are the different version numbers and the moved textures. All other changes are in the child structures.

---
+++ TR Poser/Parsing/TR3 Level Elements/TR3Level.h
@@ -0,0 +1,13 @@
+//
+//  TR3Level.h
+//  TR Poser
+//
+//  Created by Torsten Kammer on 20.08.12.
+//  Copyright (c) 2012 Torsten Kammer. All rights reserved.
+//
+
+#import "TR2Level.h"
+
+@interface TR3Level : TR2Level
+
+@end
---
+++ TR Poser/Parsing/TR3 Level Elements/TR3Level.m
@@ -0,0 +1,57 @@
+//
+//  TR3Level.m
+//  TR Poser
+//
+//  Created by Torsten Kammer on 20.08.12.
+//  Copyright (c) 2012 Torsten Kammer. All rights reserved.
+//
+
+#import "TR3Level.h"
+
+@implementation TR3Level
+
++ (NSString *)structureDescriptionSource
+{
+	return @"const bitu32=4278714424,4279763000;\
+	*Palette8 palette8;\
+	*Palette16 palette16;\
+	*TexturePage8 textureTiles8[bitu32];\
+	*TexturePage16 textureTiles16[textureTiles8.@count];\
+	bitu32 unused1;\
+	*Room rooms[bitu16];\
+	*FloorDataList floorData;\
+	substream meshData[bitu32*2];\
+	*MeshPointer meshPointers[bitu32];\
+	*Animation animations[bitu32];\
+	*StateChange stateChanges[bitu32];\
+	*AnimationDispatch animationDispatches[bitu32];\
+	*AnimationCommandList animationCommands;\
+	*MeshTree meshTrees[bitu32/4];\
+	*FrameData frames;\
+	*Moveable moveables[bitu32];\
+	*StaticMesh staticMeshes[bitu32];\
+	*SpriteTexture spriteTextures[bitu32];\
+	*SpriteSequence spriteSequences[bitu32];\
+	*Camera cameras[bitu32];\
+	*SoundSource soundSources[bitu32];\
+	*Box boxes[bitu32];\
+	bitu16 overlaps[bitu32];\
+	*Zone zones[boxes.@count];\
+	bitu16 animatedTextures[bitu32];\
+	*Texture objectTextures[bitu32];\
+	*Item items[bitu32];\
+	*Lightmap lightmap;\
+	*CinematicFrame cinematicFrames[bitu16];\
+	bitu8 demoData[bitu16];\
+	*SoundMap soundMap;\
+	*SoundDetail soundDetails[bitu32];\
+	bitu32 sampleIndices[bitu32];";
+}
+
+- (NSUInteger)gameVersion;
+{
+	return 2;
+}
+
+
+@end

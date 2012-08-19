ebccce89720e30efa4c9a60f043ab88671ac29b4
cochrane <torsten@ferroequinologist.de>
2012-08-20 01:26:34 +0200

    Specialized version of Room for Tomb Raider 3.
    Note that the final three bytes are not the room color, no matter how often TRosettaStone claims it is. You can verify this for yourself: Treat them as room color, it will look all wrong. http://www.evpopov.com/perso/dl/TRStone/TRLevelFileFormat.html gives these definitions instead.

---
+++ TR Poser/Parsing/TR3 Level Elements/TR3Room.h
@@ -0,0 +1,17 @@
+//
+//  TR3Room.h
+//  TR Poser
+//
+//  Created by Torsten Kammer on 20.08.12.
+//  Copyright (c) 2012 Torsten Kammer. All rights reserved.
+//
+
+#import "TR2Room.h"
+
+@interface TR3Room : TR2Room
+
+@property (nonatomic, assign) NSUInteger param1;
+@property (nonatomic, assign) NSUInteger unknown1;
+@property (nonatomic, assign) NSUInteger unknown2;
+
+@end
---
+++ TR Poser/Parsing/TR3 Level Elements/TR3Room.m
@@ -0,0 +1,41 @@
+//
+//  TR3Room.m
+//  TR Poser
+//
+//  Created by Torsten Kammer on 20.08.12.
+//  Copyright (c) 2012 Torsten Kammer. All rights reserved.
+//
+
+#import "TR3Room.h"
+
+#import "TRInDataStream.h"
+#import "TROutDataStream.h"
+
+@implementation TR3Room
+
+- (void)readGlobalLightState:(TRInDataStream *)stream;
+{
+	self.ambientIntensity1 = [stream readUint16];
+	self.lightMode = [stream readUint16];
+}
+- (void)writeGlobalLightState:(TROutDataStream *)stream;
+{
+	[stream appendUint16:(uint16_t) self.ambientIntensity1];
+	[stream appendUint16:(uint16_t) self.lightMode];
+}
+
+- (void)readRoomFooter:(TRInDataStream *)stream;
+{
+	self.param1 = [stream readUint8];
+	self.unknown1 = [stream readUint8];
+	self.unknown2 = [stream readUint8];
+}
+- (void)writeRoomFooter:(TROutDataStream *)stream;
+{
+	[stream appendUint8:(uint8_t) self.param1];
+	[stream appendUint8:(uint8_t) self.unknown1];
+	[stream appendUint8:(uint8_t) self.unknown2];
+}
+
+
+@end

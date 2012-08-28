2b1fbc295bffeed0a37984d165605e661c12a8df
cochrane <torsten@ferroequinologist.de>
2012-08-28 13:40:28 +0200

    Added TR3Texture with check for alpha
    TR2 doesn't use the attribute, but TR3 does, so a new texture class is
    necessary.

---
+++ TR Poser/Parsing/TR3 Level Elements/TR3Texture.h
@@ -0,0 +1,13 @@
+//
+//  TR3Texture.h
+//  TR Poser
+//
+//  Created by Torsten Kammer on 28.08.12.
+//  Copyright (c) 2012 Torsten Kammer. All rights reserved.
+//
+
+#import "TR1Texture.h"
+
+@interface TR3Texture : TR1Texture
+
+@end
---
+++ TR Poser/Parsing/TR3 Level Elements/TR3Texture.m
@@ -0,0 +1,18 @@
+//
+//  TR3Texture.m
+//  TR Poser
+//
+//  Created by Torsten Kammer on 28.08.12.
+//  Copyright (c) 2012 Torsten Kammer. All rights reserved.
+//
+
+#import "TR3Texture.h"
+
+@implementation TR3Texture
+
+- (BOOL)hasAlpha
+{
+	return self.attribute == 2;
+}
+
+@end
